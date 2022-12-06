c0ea7736da4239c5e808ad66ba16086a9f45103a
Ivan Palacios <ivpalacios47@gmail.com>
2022-12-06 14:43:51 -0400

    upload image server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
 const db = mysql.createPool({
   host: "mysql_db",
   user: "MYSQL_USER",
   password: "MYSQL_PASSWORD",
-  database: "whiteboard"
+  database: "whiteboard",
+});
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "images"),
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}.${file.mimetype.split("/")[1]}`);
+  },
 });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  multer({
+    storage,
+    dest: path.join(__dirname, "images"),
+  }).single("image")
+);
+
+
+// const upload = multer({ storage: storage });
+// app.use(express.static(path.join(_dirname, "/Images/")));
+// app.use("/", require("./Images"));
 
 app.get("/", (req, res) => {
   res.send("Hi there");
@@ -41,4 +61,14 @@ app.delete("/delete/:imageId", (req, res) => {
   });
 });
 
+app.post("/upload", function (req, res) {
+  const file = req.file;
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    console.log(error)
+  }
+  res.send(file);
+  console.log(req.file, req.body);
+});
 app.listen("3001", () => {});
